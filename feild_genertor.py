7d22e76c2fb24ff56fc08037b2fc6f85c06f063e
Michal <269996@student.pwr.edu.pl>
2022-10-19 14:42:13 +0200

    init commit

---
+++ feild_genertor.py
@@ -0,0 +1,27 @@
+
+import matplotlib as plt 
+import numpy as np
+import point
+
+# x_range = 10
+# y_range = 10
+# squer_size = 0.1
+# step = squer_size/2
+
+# plt.ylim(-y_range, y_range) 
+# plt.xlim(-x_range, x_range)
+postive_c = point(0,0)
+nagtive_c = [[1,1]]
+
+for points in postive_c:
+
+
+# x_points = np.arange(-x_range+step, x_range-step, step)
+# y_points = np.arange(-y_range+step, y_range-step, step)
+
+# for x_p in x_points:
+#     for y_p in y_points:
+#         plt.plot(0, 0, 'o')
+
+
+# plt.show()
---
+++ point.py
@@ -0,0 +1,6 @@
+class Person:
+    X = 0
+    Y = 0
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
